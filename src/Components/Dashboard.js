a7cc9a01d9309c14ef69c3c81cedeb57aefaef8a
Dylan Koss <dylanhkoss@gmail.com>
2023-03-14 14:53:04 -0500

    fixed navbar bugs

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,32 +1,14 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react'
 import { useAuth } from '../AuthContext'
 
 export default function Dashboard() {
 
-  const { logout, currentUser } = useAuth()
-  const navigate = useNavigate()
-  const [error, setError] = useState()
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    
-    try {
-      setError('')
-      await logout()
-      navigate('/login')
-    } catch {
-      setError('Failed to logout')
-    }
-  
-  }
+  const { currentUser } = useAuth()
 
   return (
     <div className='bg-gray-100 h-screen'>
       <h1 className='text-center text-3xl font-bold'>Dashboard</h1>
         <p className='text-center text-3xl py-3'>{currentUser && currentUser.email}</p>
-        {error && <p>{error}</p>}
-        <button onClick={handleSubmit} className='flex justify-center m-auto text-xl text-white bg-orange-500 rounded-md py-1 px-4 mt-4'>Logout</button>
     </div>
   )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,25 @@
 import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../AuthContext'
 
 export default function Navbar() {  
+    const { logout } = useAuth()
+    const navigate = useNavigate()
+
     const [showDropdown, setShowDropdown] = useState(false)
 
     const toggleDropdown = () => {
         setShowDropdown(!showDropdown)
+    }
+
+    const handleLogout = async () => {
+        try {
+            await logout()
+            navigate('/login')
+            setShowDropdown(false)
+        } catch(e) {
+            console.error(e)
+        }
     }
 
   return (
@@ -24,14 +39,14 @@ export default function Navbar() {
                     </svg>
                 </button>
 
-                {showDropdown && (
+                {showDropdown && 
                     <div className="origin-top-right absolute right-0 mt-28 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                     <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                        <a href="/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
-                        <a href="/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Logout</a>
+                        <Link to="/login" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</Link>
+                        <span onClick={handleLogout} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" role="menuitem">Logout</span>
                     </div>
                     </div>
-                )}
+                }
             </div>
         </div>
         </div>
