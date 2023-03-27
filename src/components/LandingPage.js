449a12d68953f404dee8ba5012f56931ca106ff8
Dylan Koss <dylanhkoss@gmail.com>
2023-03-27 16:49:29 -0500

    fixed landing page styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function LandingPage() {
   return (
-    <div className='h-screen mt-16 bg-gray-100 dark:bg-gray-900 dark:text-white'>
+    <div className='h-screen bg-gray-100 dark:bg-gray-900 dark:text-white'>
         <h1 className='py-10 text-4xl text-center'>Welcome to TrackMyOilChange</h1>
         <div className='flex justify-center pt-10 space-x-4'>
             <Link to='/signup'><button className='px-6 py-1 text-xl text-white bg-blue-500 rounded-md shadow-md'>Signup</button></Link>
